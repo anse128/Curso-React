7b6490e443d4de9ae1dd452d72279aa33f20326f
anse128 <sebastianescobarosorio@gmail.com>
2025-06-13 22:34:52 -0500

    Fragment

--- 03-counter-app/src/FirstApp.jsx
+++ 03-counter-app/src/FirstApp.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 export const FirstApp = () => {
-  return (
-    <h1>FirstApp</h1>
-  )
+    return (
+        <>
+            <h1>FirstApp</h1>
+            <p>subtitulo</p>
+        </>
+    )
 }
+
