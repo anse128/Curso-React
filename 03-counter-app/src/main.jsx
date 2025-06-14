602972e794c3159f10008ee2ee5355fa91b52934
anse128 <sebastianescobarosorio@gmail.com>
2025-06-13 22:24:50 -0500

    Segundo componente

--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import { HelloWorldApp } from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
 
 
-HelloWorldApp
+
 
 
 ReactDom.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <HelloWorldApp/>
+        <FirstApp/>
     </React.StrictMode>
 )
