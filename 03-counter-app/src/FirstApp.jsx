c77af9e8a7de04935173060a82bbc4ad990903bc
anse128 <sebastianescobarosorio@gmail.com>
2025-06-14 22:19:41 -0500

    PropTypes

--- 03-counter-app/src/FirstApp.jsx
+++ 03-counter-app/src/FirstApp.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
-const newMessage = {
-    message: 'Hola Mundo',
-    title: 'Sebastián'
-} // React no renderiza denuevo si esta afuera de FirstApp
+// import React from 'react'
+// const newMessage = {
+//     message: 'Hola Mundo',
+//     title: 'Sebastián'
+// } // React no renderiza denuevo si esta afuera de FirstApp
 
 
 
-const getResult = (a, b) => {
-    return a + b;
-}
+// const getResult = (a, b) => {
+//     return a + b;
+// }
 
-export const FirstApp = ({ title ,subTitle }) => {
-    console.log(title)
+import PropTypes from 'prop-types';
+
+export const FirstApp = ({ title, subTitle }) => {
+    // console.log(title)
     return (
         <>
             <h1>{title}</h1>
             {/* <h1>{getResult(1,2)}</h1> */}
             {/* <code>{JSON.stringify(newMessage)}</code> */}
-            <p>{subTitle+1}</p>
+            <p>{subTitle }</p>
         </>
     )
 }
 
+FirstApp.PropTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.number.isRequired
+}
