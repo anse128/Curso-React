b4af3c6f3090a1a35e03cac49489c8795bc4978e
anse128 <sebastianescobarosorio@gmail.com>
2025-06-15 00:00:22 -0500

    botones restar y reset

--- 03-counter-app/src/CounterApp.jsx
+++ 03-counter-app/src/CounterApp.jsx
@@ -4,20 +4,18 @@ export const CounterApp = ({ title, value }) => {
 
     const [counter, setCounter] = useState(0)
 
-    const handleAdd = () => {
-        // console.log(newValue)
-        // console.log(event)
-        // setCounter((c) => c + 1);
-        setCounter(counter+1);
-    }
+    const handleAdd = () => setCounter(counter + 1);
+    const handleSubstract = () => setCounter(counter - 1);
+    const handleReset = () => setCounter(value);
+
 
     return (
         <>
             <h1>{title}</h1>
             <h2>{counter}</h2>
-            <button onClick={handleAdd}>
-                +1
-            </button>
+            <button onClick={handleAdd}> +1 </button>
+            <button onClick={handleSubstract}> -1 </button>
+            <button onClick={handleReset}> Reset </button>
         </>
     )
 }
