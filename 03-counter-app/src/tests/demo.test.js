af8264c29db4af701c0ad4d2f6fbb8a55ab2c6de
anse128 <sebastianescobarosorio@gmail.com>
2025-06-15 21:22:10 -0500

    jest - Expect - toBe

--- 03-counter-app/src/tests/demo.test.js
+++ 03-counter-app/src/tests/demo.test.js
@@ -1,5 +1,14 @@
-test('Esta prueba no debe de fallar', () => {
-    if (1 === 0) {
-        throw new Error('No puede dividir entre cero')
-    }
-})
+describe('Pruebas en <Demo Component />', () => { 
+    
+    test("Esta prueba no debe de fallar", () => {
+        // 1. inicialización
+        const message1 = "Hola Mundo";
+    
+        // 2. estímulo
+        const message2 = message1.trim();
+    
+        // 3. Observar el comportamiento... esperado
+        expect(message1).toBe(message2);
+    
+    });
+ })
